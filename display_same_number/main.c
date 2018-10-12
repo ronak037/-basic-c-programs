644fbf76d961f78eca71b85bb596c2763d23c6e2
ronak037 <ronakmangal037@gmail.com>
2018-10-13 01:35:04 +0530

    My second commit

---
+++ display_same_number/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    int num;
+    printf("Enter any number ");
+    scanf("%d",&num);
+
+    printf("Now i m letting u on a secret.....\n");
+    printf("you have just enter a number %d\n",num);
+}
