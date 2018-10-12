f6c84eb46ab0777968efb23bd4d13e6f0cefd7b0
ronak037 <ronakmangal037@gmail.com>
2018-10-13 01:28:02 +0530

    My first commit

---
+++ 1st c project- increment number by 1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float num;
+    int x;
+    printf("Enter any number: ");
+    scanf("%f", &num);
+    x = num + 1;
+    printf("The value is: %f and %d\n",num,x);
+    return 0;
+}
