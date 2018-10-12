430299dd56362e2dc7865867d96685e576b955d7
ronak037 <ronakmangal037@gmail.com>
2018-10-13 01:40:10 +0530

    4th commit

---
+++ sum_of_two_numbers/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    float a;
+    float b;
+    printf ("for sum of two numbers enter first number: ");
+    scanf ("%f",&a);
+    printf ("now enter second number: ");
+    scanf ("%f",&b);
+    printf("your sum is: %f\n ",a+b);
+    return 0;
+}
+
