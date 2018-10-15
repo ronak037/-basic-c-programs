44a5e020cadd31ea2d889ec14f517a88bab8e1c8
ronak037 <ronakmangal037@gmail.com>
2018-10-16 00:56:22 +0530

    decreasing number in stars with the next row

---
+++ decreasing_star_number_loop/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i-1;j++)
+        printf(" ");
+        for(int j=1;j<=n+1-i;j++)
+        printf("*");
+        printf("\n");
+    }
+    return 0;
+}
