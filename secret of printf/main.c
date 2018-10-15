5e1cd630a9d5af1089161d319a822ab24ebb774e
ronak037 <ronakmangal037@gmail.com>
2018-10-16 00:52:11 +0530

    secret of printf

---
+++ secret of printf/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf(" %d\n",printf("hello")!=printf("world"));
+    printf(" %d\n",printf("hello")==printf("world"));
+    return 0;
+}
