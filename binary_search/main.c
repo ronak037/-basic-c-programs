db09ecb0539aa391dfbff77cddf3dc6f2b78520b
ronak037 <ronakmangal037@gmail.com>
2018-11-07 23:34:15 +0530

    Binary_search

---
+++ binary_search/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int size,flag=0;
+    printf("Enter size:");
+    scanf("%d",&size);
+    int a[size];
+    for(int i=0;i<size;i++){
+        printf("Enter %d element ",i);
+        scanf("%d",&a[i]);
+    }
+
+    int key;
+    printf("enter what number u wanna:");
+    scanf("%d",&key);
+
+    for(int i=0,j=size-1;i!=j-1;){
+        int middle;
+        middle=(i+j)/2;
+        if(key>a[middle]){
+            i=middle;
+        }
+        else if(key<a[middle]){
+            j=middle;
+        }
+        else if(key==a[middle]){
+            flag=1;
+            break;
+        }
+    }
+
+    if(flag==1){
+        printf("found");
+    }
+    else{
+        printf("not found");
+    }
+    return 0;
+}
