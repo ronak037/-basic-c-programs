919f85472fce154c6ba28424033f05ef6982b350
ronak037 <ronakmangal037@gmail.com>
2019-02-16 22:51:25 +0530

    latest commit

--- binary_search/main.c
+++ binary_search/main.c
@@ -8,7 +8,7 @@ int main()
     scanf("%d",&size);
     int a[size];
     for(int i=0;i<size;i++){
-        printf("Enter %d element ",i);
+        printf("Enter %d element ",i+1);
         scanf("%d",&a[i]);
     }
 
